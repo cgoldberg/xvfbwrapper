739f7865f287e6cacd8e2e7d39706802f2bc9800
Corey Goldberg <cgoldberg@gmail.com>
2015-09-20 11:45:52 -0400

    fixed nit in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Operating System :: Unix',
         'Operating System :: POSIX :: Linux',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
