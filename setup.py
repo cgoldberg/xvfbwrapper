6eb7158ce15154ad65d54d31f7aa3dd13215edd2
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-02-24 21:26:39 -0500

    Re-format codebase with black formatter

--- setup.py
+++ setup.py
@@ -41,5 +41,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
 )
